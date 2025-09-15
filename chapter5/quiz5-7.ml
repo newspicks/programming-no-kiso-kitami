12933f04c58abd23a77196eb1447ef04ab3b6de9
Sho Kitami <shokitami@gmail.com>
2025-09-15 17:49:23 +0900

    feature:quiz5-7追加

--- chapter5/quiz5-7.ml
+++ chapter5/quiz5-7.ml
@@ -1,20 +1,16 @@
+(* BMIを計算する *)
+(* bmi : float -> float -> float *)
 let bmi shincho taiju = taiju /. (shincho ** 2.0);;
-bmi 70.0 1.7;;
-let himan shincho taiju = "";;
+bmi 1.7 70.0
 
-let test1 = himan 1.7 70.0 = "標準";;
-let test2 = himan 1.7 80.0 = "肥満";;
-let test3 = himan 1.7 90.0 = "高度肥満";;
-let test4 = himan 1.7 50.0 = "やせ";;
-
+(* BMIから体系を判定する *)
 let himan shincho taiju = if(bmi shincho taiju) < 18.5 then "やせ"
   else if (18.5 <= (bmi shincho taiju) && (bmi shincho taiju) < 25.0) then "標準"
   else if (25.0 <= (bmi shincho taiju) && (bmi shincho taiju) < 30.0) then "肥満"
   else "高度肥満"
-  ;;
 
 
-let test1 = himan 1.7 70.0 = "標準";;
-let test2 = himan 1.7 80.0 = "肥満";;
-let test3 = himan 1.7 90.0 = "高度肥満";;
-let test4 = himan 1.7 50.0 = "やせ";;
+let test1 = himan 1.7 70.0 = "標準"
+let test2 = himan 1.7 80.0 = "肥満"
+let test3 = himan 1.7 90.0 = "高度肥満"
+let test4 = himan 1.7 50.0 = "やせ"
