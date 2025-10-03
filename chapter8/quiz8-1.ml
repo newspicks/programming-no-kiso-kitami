6ec9d204e98d60cb7c5de70cbea5b200c52096dd
Sho Kitami <shokitami@gmail.com>
2025-10-03 14:58:32 +0900

    quiz8-1

---
+++ chapter8/quiz8-1.ml
@@ -0,0 +1,33 @@
+(* 本のデータを表す型 *)
+type book_t = { 
+  title: string;     (* 本のタイトル *)
+  author: string;    (* 著者名 *)
+  publisher: string; (* 出版社名 *)
+  price: int;        (* 価格 *)
+  isbn: string;      (* ISBN *)
+}
+
+
+let wagahaiwa_neko_dearu = { 
+  title = "吾輩は猫である"; 
+  author = "夏目漱石"; 
+  publisher = "筑摩書房"; 
+  price = 400; 
+  isbn = "4-480-00101-2" 
+};;
+
+let yukiguni = { 
+  title = "雪国"; 
+  author = "川端康成"; 
+  publisher = "新潮社"; 
+  price = 500; 
+  isbn = "4-10-100101-2" 
+};;
+
+let bocchan = { 
+  title = "坊っちゃん"; 
+  author = "夏目漱石"; 
+  publisher = "岩波書店"; 
+  price = 300; 
+  isbn = "4-00-310101-2" 
+};;
