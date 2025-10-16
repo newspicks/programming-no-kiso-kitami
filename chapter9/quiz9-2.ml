9a9ddebddf5a35633a300529f86cf20844f9cde4
Sho Kitami <shokitami@gmail.com>
2025-10-17 00:22:02 +0900

    feature:quiz9-2

---
+++ chapter9/quiz9-2.ml
@@ -0,0 +1,31 @@
+#use "../chapter8/quiz8-3.ml";;
+
+
+let taro = {
+  name = "YamadaTaro";
+  height = 170.0;
+  weight = 60.0;
+  birth_month = 1;
+  birth_day = 1;
+  blood_type = "A";
+};;
+
+let sato = {
+  name = "SatoHanako";
+  height = 160.0;
+  weight = 50.0;
+  birth_month = 5;
+  birth_day = 5;
+  blood_type = "O";
+};;
+
+let hanako = {
+  name = "YamadaHanako";
+  height = 180.0;
+  weight = 70.0;
+  birth_month = 2;
+  birth_day = 2;
+  blood_type = "B";
+};;
+
+[taro; sato; hanako]
