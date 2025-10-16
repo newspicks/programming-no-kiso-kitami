15caf967c0d7410b089ff08fa8a6311ceb4de911
Sho Kitami <shokitami@gmail.com>
2025-10-17 08:46:04 +0900

    feature:9-9追加

---
+++ chapter9/quiz9-9.ml
@@ -0,0 +1,22 @@
+type ekimei_t = {
+  kanji : string;
+  kana : string; 
+  romaji : string;
+  shozoku : string;
+}
+let global_ekimei_list = [
+  { kanji = "池袋"; kana = "いけぶくろ"; romaji = "ikebukuro"; shozoku = "JR山手線" };
+  { kanji = "目白"; kana = "めじろ"; romaji = "mejiro"; shozoku = "JR山手線" };
+  { kanji = "高田馬場"; kana = "たかだのばば"; romaji = "takadanobaba"; shozoku = "JR山手線" };
+  { kanji = "新大久保"; kana = "しんおおくぼ"; romaji = "shinokubo"; shozoku = "JR山手線" };
+  { kanji = "新宿"; kana = "しんじゅく"; romaji = "shinjuku"; shozoku = "JR山手線" };
+  { kanji = "代々木"; kana = "よよぎ"; romaji = "yoyogi"; shozoku = "JR山手線" };
+  { kanji = "原宿"; kana = "はらじゅく"; romaji = "harajuku"; shozoku = "JR山手線" };
+  { kanji = "渋谷"; kana = "しぶや"; romaji = "shibuya"; shozoku = "JR山手線" };
+  { kanji = "恵比寿"; kana = "えびす"; romaji = "ebisu"; shozoku = "JR山手線" };
+  { kanji = "目黒"; kana = "めぐろ"; romaji = "meguro"; shozoku = "JR山手線" };
+  { kanji = "五反田"; kana = "ごたんだ"; romaji = "gotanda"; shozoku = "JR山手線" };
+  { kanji = "大崎"; kana = "おおさき"; romaji = "osaki"; shozoku = "JR山手線" };
+  { kanji = "品川"; kana = "しながわ"; romaji = "shinagawa"; shozoku = "JR山手線" };
+  { kanji = "田町"; kana = "たまち"; romaji = "tamachi"; shozoku = "丸の内線"};
+]
