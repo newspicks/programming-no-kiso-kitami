671d43efacf2ade1f6337a3ec2077c72bf33191c
Sho Kitami <shokitami@gmail.com>
2025-10-17 01:04:41 +0900

    feature:quiz9-4

---
+++ chapter9/quiz9-4.ml
@@ -0,0 +1,9 @@
+(* 整数のリストを受け取ったらその長さを返す *)
+(* length : int list -> int *)
+let rec length lst = match lst with
+    [] -> 0
+    | first :: rest -> 1 + length rest;;
+
+  let test1 = length [] = 0;;
+  let test2 = length [1] = 1;;
+  let test3 = length [2; 1; 6; 4; 7] = 5;;
