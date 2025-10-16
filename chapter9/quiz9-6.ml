e5356e8b3a11ef6b32791b6ceac606e45b515e8d
Sho Kitami <shokitami@gmail.com>
2025-10-17 01:14:49 +0900

    feature:quiz9-6対応

---
+++ chapter9/quiz9-6.ml
@@ -0,0 +1,11 @@
+(* 文字列のリストを受け取ったらその中の要素を前から順に全て結合した文字列を返す *)
+(* concat : string list -> string *)
+let rec concat lst = match lst with
+    [] -> ""
+    | first :: rest -> first ^ concat rest;;
+
+
+let test1 = concat [] = "";;
+let test2 = concat ["春"] = "春";;
+let test3 = concat ["春"; "夏"] = "春夏";;
+let test4 = concat ["春"; "夏"; "秋"; "冬"] = "春夏秋冬";;
