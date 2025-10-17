c7b21cb2f61c6a6103de338dcf4ba23fa54c317a
Sho Kitami <shokitami@gmail.com>
2025-10-17 13:22:31 +0900

    quiz10-2対応

---
+++ chapter10/quiz10-2.ml
@@ -0,0 +1,11 @@
+#use "./quiz10-1.ml";;
+
+(* 目的: 整数を配列を受け取り、それを照準に整列したリストを返す *)
+(* insert : int list -> int -> int list *)
+let rec ins_sort lst = match lst with
+    [] -> []
+  | first :: rest -> insert (ins_sort rest) first
+
+let test4 = ins_sort [] = [];;
+let test5 = ins_sort [3] = [3];;
+let test6 = ins_sort [4;2;5;1;3] = [1;2;3;4;5];;
