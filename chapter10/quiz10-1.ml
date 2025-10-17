362c5987f1e5bf05e6da67e6cd516062e7b62a7f
Sho Kitami <shokitami@gmail.com>
2025-10-17 13:16:01 +0900

    quiz10-1

---
+++ chapter10/quiz10-1.ml
@@ -0,0 +1,14 @@
+
+(* 目的: 整数 x を昇順に保つようにリスト lst に挿入する *)
+(* insert : int -> int list -> int list *)
+let rec insert lst x = match lst with
+    [] -> [x]
+  | first :: rest ->
+      if x <= first then
+        x :: lst
+      else
+        first :: insert rest x
+
+let test1 = insert [] 2 = [2];;
+let test2 = insert [1;3;4] 2 = [1;2;3;4];;
+let test3 = insert [1;3;4;7;8;] 5 = [1;3;4;5;7;8];;
