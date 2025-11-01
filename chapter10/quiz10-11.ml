3ee39576a1daefd57785f1301ebd74f230d37079
Sho Kitami <shokitami@gmail.com>
2025-11-01 14:25:16 +0900

    feature:quiz10-11対応

--- chapter10/quiz10-11.ml
+++ chapter10/quiz10-11.ml
@@ -1,11 +1,19 @@
+#use "../chapter9/quiz9-10.ml"
 
 (* 目的: 2つの駅名の距離をかえす*)
 (* get_ekikan_kyori -> string -> string -> ekimei_t list -> float *)
 let rec get_ekikan_kyori ekimei1 ekimei2 global_ekimei_list = match global_ekimei_list with
   [] -> infinity
-  | {kiten = k; shuten = s; kyori = kr} :: rest -> 
-    if (ekimei1 = k && ekimei2 = s) || (ekimei = s && ekimei2 = k)
+  | {kiten = k; shuten = s; keiyu = ke; kyori = kr; jikan = j} :: rest ->
+    if (ekimei1 = k && ekimei2 = s) || (ekimei1 = s && ekimei2 = k)
       then kr
     else get_ekikan_kyori ekimei1 ekimei2 rest
 
-  let test = get_ekikan_kyori "代々木上原" "明大前" global_ekikan_list = 2.5
+let test1 = get_ekikan_kyori "代々木上原" "代々木公園" global_ekikan_list = 1.0
+let test2 = get_ekikan_kyori "代々木公園" "代々木上原"  global_ekikan_list = 1.0
+let test3 = get_ekikan_kyori "代々木上原" "明治神宮前" global_ekikan_list = infinity
+let test4 = get_ekikan_kyori "明治神宮前" "代々木上原" global_ekikan_list = infinity
+
+let test5 = get_ekikan_kyori "中野坂上" "新中野" global_ekikan_list = 1.1
+let test6 = get_ekikan_kyori "ななしのごんべえ" "中野坂上" global_ekikan_list = infinity
+let test7 = get_ekikan_kyori "中野坂上" "ななしのごんべえ" global_ekikan_list = infinity
