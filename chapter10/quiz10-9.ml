ac1920e9a26b12dcf188225ae460c9a9f8b61b76
Sho Kitami <shokitami@gmail.com>
2025-11-01 13:39:57 +0900

    feature:quiz10-9対応

---
+++ chapter10/quiz10-9.ml
@@ -0,0 +1,13 @@
+(* 目的:2つのリストを受け取ってきたらそれらの長さが同じかどうか判定する *)
+(* equal_length : 'a list -> 'a list -> bool *)
+let rec equal_length lst1 lst2 = match (lst1, lst2) with 
+  ([], []) -> true
+  | (first1 :: rest1, []) -> false
+  | ([], first2 :: rest2) -> false
+  | (first1 :: rest1, first2 :: rest2) ->
+      equal_length rest1 rest2
+
+let test1= equal_length [] [] = true
+let test2= equal_length [1;2;3] [] = false
+let test3= equal_length [] [4;5;6] = false
+let test4= equal_length [1;2;3] [4;5;6] = true
