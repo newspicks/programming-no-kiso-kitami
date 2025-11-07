3a9dd9509686d9131b3fcbe1df72dd85d2a9bcbd
Sho Kitami <shokitami@gmail.com>
2025-11-07 16:02:59 +0900

    fix:一部修正

--- chapter10/quiz10-7.ml
+++ chapter10/quiz10-7.ml
@@ -6,14 +6,11 @@ let rec ketsueki_shukei lst = match lst with
   [] -> (0, 0, 0, 0)
   | {blood_type = bt;} :: rest ->
     let (a, b, o, ab) = ketsueki_shukei rest in
-    if bt = "A" then
-      (a + 1, b, o, ab)
-    else if bt = "B" then
-      (a, b + 1, o, ab)
-    else if bt = "O" then
-      (a, b, o + 1, ab)
-    else
-      (a, b, o, ab + 1)
+    match bt with
+        "A" ->  (a + 1, b, o, ab) 
+        | "B" -> (a, b + 1, o, ab)
+        | "O" -> (a, b, o + 1, ab)
+        | _ -> (a, b, o, ab + 1)
 
 let test1 = ketsueki_shukei [] = (0, 0, 0, 0)
 let test2 = ketsueki_shukei [person1] = (1, 0, 0, 0)
--- chapter10/quiz10-9.ml
+++ chapter10/quiz10-9.ml
@@ -1,5 +1,5 @@
 (* 目的:2つのリストを受け取ってきたらそれらの長さが同じかどうか判定する *)
-(* equal_length : 'a list -> 'a list -> bool *)
+(* equal_length : 'a list -> 'b list -> bool *)
 let rec equal_length lst1 lst2 = match (lst1, lst2) with 
   ([], []) -> true
   | (first1 :: rest1, []) -> false
@@ -11,3 +11,4 @@ let test1= equal_length [] [] = true
 let test2= equal_length [1;2;3] [] = false
 let test3= equal_length [] [4;5;6] = false
 let test4= equal_length [1;2;3] [4;5;6] = true
+let test5 = equal_length ["A";"B";"C"] [4;5;6] = true
