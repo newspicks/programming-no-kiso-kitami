70e48205edf7971a3489038cf60a28d4b21ef03b
Sho Kitami <shokitami@gmail.com>
2025-11-21 08:23:29 +0900

    fix:駅名

--- chapter13/quiz13-6.ml
+++ chapter13/quiz13-6.ml
@@ -1,6 +1,6 @@
 #use "../metro_data/metro_data.ml"
 
-(* 目的：漢字の駅名をふたつと ekikan_t list を受け取り、駅間の距離を返す *)
+(* 目的:漢字の駅名とekikan_tのリストを受け取り、駅間の距離を返す *)
 (* get_ekikan_kyori: string -> string -> ekikan_t list -> float *)
 let rec get_ekikan_kyori eki1 eki2 lst = match lst with
   [] -> infinity
@@ -9,17 +9,16 @@ let rec get_ekikan_kyori eki1 eki2 lst = match lst with
       then ky
       else get_ekikan_kyori eki1 eki2 rest
 
-let test1 = get_ekikan_kyori "東京" "表参道" global_ekikan_list = infinity
+let test1 = get_ekikan_kyori "代々木上原" "表参道" global_ekikan_list = infinity
 let test2 = get_ekikan_kyori "渋谷" "表参道" global_ekikan_list = 1.3
-let test3 = get_ekikan_kyori "表参道" "外苑前" global_ekikan_list = 0.7
+let test3 = get_ekikan_kyori "乃木坂" "赤坂" global_ekikan_list = 1.1
 
 let ikebukuro = {namae="池袋"; saitan_kyori = infinity; temae_list = []} 
-let eki2 = {namae="新大塚"; saitan_kyori = 1.2; temae_list = ["新大塚"; "茗荷谷"]} 
-let eki3 = {namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]} 
+let otemachi = {namae="大手町"; saitan_kyori = 0.8; temae_list = ["大手町"; "日本橋"]} 
+let myogadani = {namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]} 
 
-(* 直前に確定した駅 p と　未確定の駅 q を受け取り、ふたつが直接つながっているか調べ、 *)
-(* つながっていない場合は q をそのまま返し、 *)
-(* つながっている場合は必要に応じて最短距離とリストを更新して返す *)
+(* 目的:直前に確定した駅 p と未確定の駅 q を受け取り、ふたつが直接つながっているか調べ、 *)
+(* つながっていない場合は q をそのまま返し、つながっている場合は必要に応じて最短距離とリストを更新して返す *)
 (* koushin1: eki_t -> eki_t -> eki_t *)
 let koushin1 p q = match (p, q) with
   ({namae = pn; saitan_kyori = pk; temae_list = pl},
@@ -31,6 +30,6 @@ let koushin1 p q = match (p, q) with
       then {namae = qn; saitan_kyori = pk +. kyori; temae_list = qn :: pl}
       else q
 
-let test5 = koushin1 eki3 ikebukuro = eki1 
-let test6 = koushin1 eki3 eki2 = eki2 
-let test7 = koushin1 eki3 eki3 = eki3 
+let test5 = koushin1 myogadani ikebukuro = ikebukuro 
+let test6 = koushin1 myogadani otemachi = otemachi 
+let test7 = koushin1 myogadani myogadani = myogadani 
