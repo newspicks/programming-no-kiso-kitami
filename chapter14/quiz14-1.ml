154899945a8759b402954742180ca482cc554004
Sho Kitami <shokitami@gmail.com>
2025-11-23 15:55:26 +0900

    feature:quiz14-1対応

---
+++ chapter14/quiz14-1.ml
@@ -0,0 +1,11 @@
+
+(* 目的:整数のリストを受け取り、偶数の要素をリストとしてかえす *)
+(* even -> int list -> int list *)
+let even lst = List.filter (fun x -> x mod 2 = 0) lst
+
+let test1 = even [] = []
+let test2 = even [1] = [1]
+let test3 = even [2] = [2]
+let test4 = even [1; 2] = [2]
+let test5 = even [-1] = []
+let test6 = even [-2] = [-2]
